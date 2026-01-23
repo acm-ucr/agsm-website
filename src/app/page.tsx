7d482dd2ba2d3100c1e32a445ed06070664372b2
paoloprogram12 <sethpsalazar@gmail.com>
2026-01-22 18:49:50 -0800

    finished component to match the example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Example from "@/components/Example";
 
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <Example text1="adjust" text2="text2" text3="these" />
+    <div className="bg-agsm-blue-300 flex h-screen w-screen items-center justify-center">
+      <Example text1="Adjust" text2="Props" text3="These" />
     </div>
   );
 };
--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,10 +1,19 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="flex w-3/4 items-center gap-1 bg-red-200 p-10">
-      <div className="w-1/2 bg-green-200 p-5 text-left">{props.text1}</div>
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="w-full bg-green-200 p-5 text-left">props.text2</div>
-        <div className="w-full bg-green-200 p-5 text-right">{props.text2}</div>
+    <div className="bg-agsm-blue-300 flex w-2/3 flex-col items-center gap-4 p-10">
+      <div className="bg-agsm-blue-200 w-1/2 p-5 text-center text-xs text-white">
+        {props.text1}
+      </div>
+      <div className="flex w-1/2 flex-row gap-5">
+        <div className="bg-agsm-blue-100 w-full rounded-lg p-5 text-center text-xs font-bold">
+          {props.text3}
+        </div>
+        <div className="bg-agsm-blue-100 w-full rounded-lg p-5 text-center text-xs">
+          {props.text3}
+        </div>
+      </div>
+      <div className="bg-agsm-yellow-200 w-1/2 p-5 text-center text-xl font-bold">
+        {props.text2}
       </div>
     </div>
   );
