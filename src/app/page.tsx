7b0ec616070ee756a74ce80f21757da0437e324c
charlesluo06 <charlesluo06@gmail.com>
2026-01-22 19:09:30 -0800

    small fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import Footer from "@/components/Footer";
+import Example from "@/components/Example";
 
 const Home = () => {
-  return <Footer />;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Example text1="adjust" text2="text2" text3="these" />
+    </div>
+  );
 };
 
 export default Home;
