ab5dce2bb7dc3d4eef9c6b45a2ebad3dc40556ad
Jason Nguyen <icytea@Jasons-Mac-mini.local>
2026-01-27 23:57:06 -0800

    Created Upcoming Events Card

---
+++ src/components/events/UpcomingEventsCard.tsx
@@ -0,0 +1,21 @@
+interface UpcomingEventCardType {
+  month: string;
+  date: string;
+  eventName: string;
+  schoolName: string;
+  time: string;
+  description: string;
+}
+
+const UpcomingEventCard = ({
+  month,
+  date,
+  eventName,
+  schoolName,
+  time,
+  description,
+}: UpcomingEventCardType) => {
+  return <div></div>;
+};
+
+export default UpcomingEventCard;
