e00ed95496d011868cca5db6e202e33e45d9db24
paoloprogram12 <sethpsalazar@gmail.com>
2026-02-01 21:10:16 -0800

    created testimonials page

--- src/app/alumni/page.tsx
+++ src/app/alumni/page.tsx
@@ -2,9 +2,9 @@ import Testimonials from "@/components/alumni/Testimonials";
 
 const Alumni = () => {
   return (
-  <div className="flex h-screen w-screen items-center justify-center">
-    <Testimonials/>
-  </div>
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Testimonials />
+    </div>
   );
 };
 
--- src/components/alumni/Testimonials.tsx
+++ src/components/alumni/Testimonials.tsx
@@ -1,20 +1,29 @@
-const Testimonials = (props: {  }) => {
+const Testimonials = (props: {}) => {
   return (
-    <div className="flex flex-col gap-8 w-full">
-        <div className="bg-gradient-to-r from-white to-agsm-blue-100 p-4">
-            <h2 className="text-center text-4xl font-bold text-agsm-blue-200 font-agsm-main">TESTIMONIALS</h2>
-            </div>
-        <div className="bg-agsm-yellow-100 flex flex-row max-w-2xl mx-auto shadow-xl p-6 gap-4">
-            <img src="board/Sheetal.webp" alt="sheetal" className="w-40 h-40 rounded-lg object-cover"/>
-            <div className="p-4 flex flex-col gap-4">
-                <p className="text-agsm-blue-200 font-bold font-agsm-main text-2xl">Name</p>
-                <h2 className="font-agsm-main">"Lorem ipsum dolor sit amet consectetur 
-                    adipisicing elit. Illum dolorum recusandae, 
-                    nisi voluptate ipsa nihil beatae animi voluptatum 
-                    quis eligendi. Cumque ipsam nesciunt, vitae dolor nihil 
-                    illo? Mollitia, molestiae cumque!"</h2>
-            </div>
+    <div className="flex w-full flex-col gap-8">
+      <div className="to-agsm-blue-100 bg-gradient-to-r from-white p-4">
+        <h2 className="text-agsm-blue-200 font-agsm-main text-center text-4xl font-bold">
+          TESTIMONIALS
+        </h2>
+      </div>
+      <div className="bg-agsm-yellow-100 mx-auto flex max-w-2xl flex-row gap-4 p-6 shadow-xl">
+        <img
+          src="board/Sheetal.webp"
+          alt="sheetal"
+          className="h-40 w-40 rounded-lg object-cover"
+        />
+        <div className="flex flex-col gap-4 p-4">
+          <p className="text-agsm-blue-200 font-agsm-main text-2xl font-bold">
+            Name
+          </p>
+          <h2 className="font-agsm-main">
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+            dolorum recusandae, nisi voluptate ipsa nihil beatae animi
+            voluptatum quis eligendi. Cumque ipsam nesciunt, vitae dolor nihil
+            illo? Mollitia, molestiae cumque!"
+          </h2>
         </div>
+      </div>
     </div>
   );
 };
