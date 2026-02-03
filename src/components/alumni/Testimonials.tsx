5f346402f17c78ef25581fa144205a52d605dfaf
paoloprogram12 <sethpsalazar@gmail.com>
2026-02-02 16:21:46 -0800

    Changed h2 to p and removed img

--- src/components/alumni/Testimonials.tsx
+++ src/components/alumni/Testimonials.tsx
@@ -1,14 +1,17 @@
+import Image from "next/image";
+import AlumniImage from "../../../public/board/Sheetal.webp";
+
 const Testimonials = () => {
   return (
     <div className="flex w-full flex-col gap-8">
       <div className="to-agsm-blue-100 bg-gradient-to-r from-white p-4">
-        <h2 className="text-agsm-blue-200 font-agsm-main text-center text-4xl font-bold">
+        <p className="text-agsm-blue-200 font-agsm-main text-center text-4xl font-bold">
           TESTIMONIALS
-        </h2>
+        </p>
       </div>
       <div className="bg-agsm-yellow-100 mx-auto flex max-w-2xl flex-row gap-4 p-6 shadow-xl">
-        <img
-          src="board/Sheetal.webp"
+        <Image
+          src={AlumniImage}
           alt="sheetal"
           className="h-40 w-40 rounded-lg object-cover"
         />
@@ -16,12 +19,12 @@ const Testimonials = () => {
           <p className="text-agsm-blue-200 font-agsm-main text-2xl font-bold">
             Name
           </p>
-          <h2 className="font-agsm-main">
+          <p className="font-agsm-main">
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
             dolorum recusandae, nisi voluptate ipsa nihil beatae animi
             voluptatum quis eligendi. Cumque ipsam nesciunt, vitae dolor nihil
             illo? Mollitia, molestiae cumque!"
-          </h2>
+          </p>
         </div>
       </div>
     </div>
