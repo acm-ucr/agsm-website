ef473f0faf73b13e6a88a0eb23d8ff75b7cd8172
Larry Zhou <zhoularry15@gmail.com>
2026-02-02 20:46:41 -0800

    completed the About Us Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Example from "@/components/Example";
+import AboutUs from "@/components/landing/AboutUs";
 
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <Example text1="adjust" text2="text2" text3="these" />
+    <div className="bg-agsm-yellow-100 font-agsm-main flex h-screen w-screen">
+      <AboutUs
+        title="ABOUT US"
+        title2="Launching the Next Generation of Product Leaders"
+        description="Whether you're exploring PM roles for the first time or preparing for your dream teach internship, we provide the resources, hands-on experience, and network you need to succeed. From curated learning materials to case competitions and alumni mentorship, we're building the pathway from zero experience to product leadership."
+      />
     </div>
   );
 };
--- src/components/landing/AboutUs.tsx
+++ src/components/landing/AboutUs.tsx
@@ -1,7 +1,41 @@
-// const AboutUs = () => {
-//   return (
+import Image from "next/image";
+import AboutUsIm from "@/public/about_us.webp";
+import Logo from "@/public/logo.webp";
 
-//   );
-// };
+interface AboutUsProps {
+  title: string;
+  description: string;
+  title2: string;
+}
 
-// export default AboutUs;
+const AboutUs = ({ title, description, title2 }: AboutUsProps) => {
+  return (
+    <div className="mt-3 flex w-screen flex-col items-center gap-20 p-8">
+      <h1 className="text-agsm-blue-200 text-7xl font-bold">{title}</h1>
+      <div className="items-top flex w-4/5 flex-row justify-center gap-x-20 text-left">
+        <div className="flex w-3/7 flex-col items-center gap-15">
+          <h2 className="text-agsm-blue-200 mt-20 w-12/11 gap-20 p-2 text-5xl">
+            {title2}
+          </h2>
+          <p className="w-12/11 pr-20 text-2xl">{description}</p>
+        </div>
+        <div className="h-7/6 w-1/3 overflow-hidden rounded-lg">
+          <Image
+            src={AboutUsIm}
+            alt="Students and faculty members in a meeting"
+            className="h-full w-full items-center object-cover"
+          />
+          <div className="">
+            <Image
+              src={Logo}
+              alt="AGSM Logo"
+              className="absolute bottom-30 left-360 size-55"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
