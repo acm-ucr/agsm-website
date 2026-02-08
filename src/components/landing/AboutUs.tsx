f425b5ebe5f0a0f7d4cac53956c054d4ac22cf80
Larry Zhou <zhoularry15@gmail.com>
2026-02-07 23:12:08 -0800

    changed h1 to p, removed brackets around string, and change hard-coded px to fractions

--- src/components/landing/AboutUs.tsx
+++ src/components/landing/AboutUs.tsx
@@ -5,19 +5,21 @@ import Logo from "@/public/logo.webp";
 const AboutUs = () => {
   return (
     <div className="mt-3 flex w-full flex-col items-center gap-18 p-5">
-      <h1 className="text-agsm-blue-200 text-[50px] font-bold">{"ABOUT US"}</h1>
+      <p className="text-agsm-blue-200 text-6xl font-bold">ABOUT US</p>
       <div className="items-top flex w-4/5 flex-row justify-center gap-x-15 text-left">
         <div className="flex w-3/5 flex-col items-center gap-15">
           <p className="text-agsm-blue-200 mt-7 p-2 text-4xl font-bold">
-            {"Launching the Next Generation of Product Leaders"}
+            Launching the Next Generation of Product Leaders
           </p>
           <p className="w-full pr-20 text-xl">
-            {
-              "Whether you're exploring PM roles for the first time or preparing for your dream teach internship, we provide the resources, hands-on experience, and network you need to succeed. From curated learning materials to case competitions and alumni mentorship, we're building the pathway from zero experience to product leadership."
-            }
+            Whether you're exploring PM roles for the first time or preparing
+            for your dream teach internship, we provide the resources, hands-on
+            experience, and network you need to succeed. From curated learning
+            materials to case competitions and alumni mentorship, we're building
+            the pathway from zero experience to product leadership.
           </p>
         </div>
-        <div className="h-[450px] w-1/2 overflow-hidden rounded-lg">
+        <div className="h-4/3 w-1/2 overflow-hidden rounded-lg">
           <Image
             src={AboutUsIm}
             alt="Students and faculty members in a meeting"
