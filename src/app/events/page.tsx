090829950ff9788adc0e07dbfd93af39570933b3
“Alaya” <alaya.sanai@icloud.com>
2026-02-07 22:29:21 -0800

    completed past events

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,5 +1,16 @@
+import PastEvents from "@/components/events/PastEvents";
+import eventphoto from "@/public/about_us.webp";
 const Events = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <PastEvents
+        text1="PAST EVENTS"
+        text2="Event Name"
+        text3="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        photo={eventphoto}
+      />
+    </div>
+  );
 };
 
 export default Events;
--- src/components/events/PastEvents.tsx
+++ src/components/events/PastEvents.tsx
@@ -1,7 +1,32 @@
-// const PastEvents = () => {
-//   return (
+import Image, { StaticImageData } from "next/image";
+const PastEvents = (props: {
+  text1: string;
+  text2: string;
+  text3: string;
+  photo: StaticImageData;
+}) => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 pt-35">
+      <div className="to-agsm-blue-100 text-agsm-blue-200 font-agsm-main flex h-20 w-full items-center justify-center bg-gradient-to-r from-white from-0% to-80% text-4xl font-extrabold">
+        {props.text1}
+      </div>
+      <div className="bg-agsm-yellow-100 mt-10 mb-10 flex h-80 w-3/5 flex-row justify-center gap-4 border p-10 shadow-lg shadow-gray-500">
+        <div className="relative flex h-60 w-4xl flex-row">
+          <Image
+            src={props.photo}
+            alt={"photo of a lecture room with students in it"}
+            className="rounded-lg"
+          />
+        </div>
+        <div className="flex flex-col">
+          <div className="text-agsm-blue-200 mt-5 flex w-2/3 flex-col gap-4 text-2xl font-extrabold">
+            {props.text2}
+          </div>
+          <div className="text-l mt-5 text-gray-600">{props.text3}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-//   );
-// };
-
-// export default PastEvents;
+export default PastEvents;
