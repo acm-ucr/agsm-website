47fe27d0e62da418896ac7d11f4b8a1d2daec706
charlesluo06 <charlesluo06@gmail.com>
2026-02-16 01:18:20 -0800

    some small changes

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,5 +1,20 @@
+"use client";
+
+import { Calendar } from "@/components/ui/calendar";
+import { useState } from "react";
+
 const Events = () => {
-  return <div>Hello World</div>;
+  const [date, setDate] = useState<Date | undefined>(new Date());
+  return (
+    <div className="bg-agsm-yellow-100 font-agsm-main flex h-screen w-screen">
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="mx-auto w-1/2"
+      />
+    </div>
+  );
 };
 
 export default Events;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,12 @@
 "use client";
 
-import { Calendar } from "@/components/ui/calendar";
-import { useState } from "react";
 import AboutUs from "@/components/landing/AboutUs";
 
 const Home = () => {
-  const [date, setDate] = useState<Date | undefined>(new Date());
-
   return (
 
     <div className="bg-agsm-yellow-100 font-agsm-main flex h-screen w-screen">
       <AboutUs />
-      <Calendar
-        mode="single"
-        selected={date}
-        onSelect={setDate}
-        className="mx-auto w-1/2"
-      />
     </div>
   );
 };
