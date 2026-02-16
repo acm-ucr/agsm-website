f25cf4a4f604ac98533f4183b06cec2a116edc9f
charlesluo06 <charlesluo06@gmail.com>
2026-02-16 02:03:37 -0800

    implemented calendar

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,17 +1,12 @@
 "use client";
 
 import { Calendar } from "@/components/ui/calendar";
-import { useState } from "react";
 
 const Events = () => {
-  const [date, setDate] = useState<Date | undefined>(new Date());
   return (
     <div className="bg-agsm-yellow-100 font-agsm-main flex h-screen w-screen">
       <Calendar
-        mode="single"
-        selected={date}
-        onSelect={setDate}
-        className="mx-auto w-1/2"
+        className="mx-auto w-1/2 mt-15"
       />
     </div>
   );
--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -33,7 +33,7 @@ function Calendar({
     <DayPicker
       showOutsideDays={showOutsideDays}
       className={cn(
-        "bg-background text-agsm-blue-200 group/calendar [--cell-size:--spacing(8)] [[data-slot=card-content]_&]:bg-transparent [[data-slot=popover-content]_&]:bg-transparent",
+        "text-agsm-blue-200 group/calendar [--cell-size:--spacing(8)] [[data-slot=card-content]_&]:bg-transparent [[data-slot=popover-content]_&]:bg-transparent",
         String.raw`rtl:**:[.rdp-button\_next>svg]:rotate-180`,
         String.raw`rtl:**:[.rdp-button\_previous>svg]:rotate-180`,
         className,
@@ -92,7 +92,7 @@ function Calendar({
         ),
         table: "w-full border-collapse",
         weekdays: cn(
-          "flex rounded-t-xl uppercase bg-agsm-blue-100 border-2 pt-4",
+          "flex rounded-t-xl uppercase bg-agsm-blue-150 border border-[#A2A2A2] pt-4",
           defaultClassNames.weekdays,
         ),
         weekday: cn(
@@ -109,7 +109,7 @@ function Calendar({
           defaultClassNames.week_number,
         ),
         day: cn(
-          "relative w-full h-full border-1 p-0 text-center [&:last-child[data-selected=true]_button]:rounded-r-md group/day aspect-square select-none",
+          "relative w-full h-full font-bold border-[1px] border-[#A2A2A2]/50 pl-2 pt-2 [&:last-child[data-selected=true]_button]:rounded-r-md group/day aspect-square select-none",
           props.showWeekNumber
             ? "[&:nth-child(2)[data-selected=true]_button]:rounded-l-md"
             : "[&:first-child[data-selected=true]_button]:rounded-l-md",
@@ -122,7 +122,7 @@ function Calendar({
         range_middle: cn("rounded-none", defaultClassNames.range_middle),
         range_end: cn("rounded-r-md bg-accent", defaultClassNames.range_end),
         today: cn(
-          "bg-agsm-blue-100 text-agsm-blue-200/70 data-[selected=true]:rounded-none",
+          "bg-agsm-blue-150 text-agsm-blue-200/65 data-[selected=true]:rounded-none",
           defaultClassNames.today,
         ),
         outside: cn(
@@ -216,7 +216,7 @@ function CalendarDayButton({
       data-range-end={modifiers.range_end}
       data-range-middle={modifiers.range_middle}
       className={cn(
-        "data-[selected-single=true]:bg-primary data-[selected-single=true]:text-primary-foreground data-[range-middle=true]:bg-accent data-[range-middle=true]:text-accent-foreground data-[range-start=true]:bg-primary data-[range-start=true]:text-primary-foreground data-[range-end=true]:bg-primary data-[range-end=true]:text-primary-foreground hover:text-agsm-blue-200/70 group-data-[focused=true]/day:border-ring group-data-[focused=true]/day:ring-ring/50 dark:hover:text-accent-foreground hover:bg-agsm-blue-100 text-md flex aspect-square size-auto w-full min-w-(--cell-size) items-start justify-start p-2 leading-none font-bold group-data-[focused=true]/day:relative group-data-[focused=true]/day:z-10 group-data-[focused=true]/day:ring-[3px] hover:rounded-none data-[range-end=true]:rounded-md data-[range-end=true]:rounded-r-md data-[range-middle=true]:rounded-none data-[range-start=true]:rounded-md data-[range-start=true]:rounded-l-md [&>span]:text-xs [&>span]:opacity-70",
+        "data-[selected-single=true]:bg-primary data-[selected-single=true]:text-primary-foreground data-[range-middle=true]:bg-accent data-[range-middle=true]:text-accent-foreground data-[range-start=true]:bg-primary data-[range-start=true]:text-primary-foreground data-[range-end=true]:bg-primary data-[range-end=true]:text-primary-foreground hover:text-agsm-blue-200/70 group-data-[focused=true]/day:border-ring group-data-[focused=true]/day:ring-ring/50 dark:hover:text-accent-foreground hover:bg-agsm-blue-150 flex aspect-square size-auto w-full min-w-(--cell-size) items-start justify-start p-2 leading-none font-bold group-data-[focused=true]/day:relative group-data-[focused=true]/day:z-10 group-data-[focused=true]/day:ring-[3px] hover:rounded-none data-[range-end=true]:rounded-md data-[range-end=true]:rounded-r-md data-[range-middle=true]:rounded-none data-[range-start=true]:rounded-md data-[range-start=true]:rounded-l-md [&>span]:text-xs [&>span]:opacity-70",
         defaultClassNames.day,
         className,
       )}
