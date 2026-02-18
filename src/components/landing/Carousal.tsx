5f904ad25fc202b736357bb9021c6b4767b03b3f
jason-ngn <bennguyen20122006@gmail.com>
2026-02-17 19:07:02 -0800

    Created the carousel gallery

--- src/components/landing/Carousal.tsx
+++ src/components/landing/Carousal.tsx
@@ -14,54 +14,66 @@ import Image4 from "@/public/carousal/4.png";
 import Image5 from "@/public/carousal/5.png";
 import leftArrow from "@/public/carousal/left_arrow.svg";
 import rightArrow from "@/public/carousal/right_arrow.svg";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const Carousal = () => {
   const imageArray = [Image1, Image2, Image3, Image4, Image5];
 
   const [carouselApi, setCarouselApi] = useState<CarouselApi | null>(null);
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    if (!carouselApi) return;
-    carouselApi.on("select", () => {
-      setCurrentIndex(carouselApi.selectedScrollSnap());
-    });
-  }, [carouselApi]);
 
   return (
     <div className="p-8">
       <div className="text-agsm-blue-200 flex items-center justify-center text-6xl font-bold">
         GALLERY
       </div>
-      <div className="flex justify-center gap-4 p-8">
-        <button
-          onClick={() => carouselApi?.scrollTo(currentIndex - 1)}
-          className="cursor-pointer"
-        >
-          <Image src={leftArrow} alt="Left Arrow" />
-        </button>
+      <div className="relative mx-auto w-full overflow-hidden p-8">
+        {/* Left fade overlay */}
+        {/*<div className="from-agsm-yellow-100 pointer-events-none absolute top-0 left-0 z-10 h-full w-100 border-2 border-red-500 bg-gradient-to-r to-transparent"></div>*/}
+
+        {/* Right fade overlay */}
+        {/*<div className="from-agsm-yellow-100 pointer-events-none absolute top-0 right-0 z-10 h-full w-100 border-2 border-red-500 bg-gradient-to-l to-transparent"></div>*/}
+
+        <div className="absolute top-0 left-0 z-20 flex h-full w-16 items-center justify-start md:w-32">
+          <div className="from-agsm-yellow-100 pointer-events-none absolute inset-0 bg-gradient-to-r to-transparent" />
+          <button
+            onClick={() => carouselApi?.scrollPrev()}
+            className="z-20 cursor-pointer"
+          >
+            <Image src={leftArrow} alt="Left Arrow" />
+          </button>
+        </div>
+
         <Carousel
           setApi={setCarouselApi}
           opts={{
             loop: true,
-            align: "start",
+            align: "center",
           }}
+          className="w-full"
         >
-          <CarouselContent className="-ml-8">
+          <CarouselContent className="-ml-10">
             {imageArray.map((image, index) => (
-              <CarouselItem key={index} className="basis-1/3 pl-8">
-                <Image src={image} alt="Image" className="rounded-md" />
+              <CarouselItem
+                key={index}
+                className="basis-full pl-10 md:basis-1/3"
+              >
+                <div className="relative overflow-hidden rounded-xl">
+                  <Image src={image} alt="Image" className="object-cover" />
+                </div>
               </CarouselItem>
             ))}
           </CarouselContent>
         </Carousel>
-        <button
-          onClick={() => carouselApi?.scrollTo(currentIndex + 1)}
-          className="cursor-pointer"
-        >
-          <Image src={rightArrow} alt="Right Arrow" />
-        </button>
+
+        <div className="absolute top-0 right-0 z-20 flex h-full w-16 items-center justify-end md:w-32">
+          <div className="from-agsm-yellow-100 pointer-events-none absolute inset-0 bg-gradient-to-l to-transparent" />
+          <button
+            onClick={() => carouselApi?.scrollNext()}
+            className="z-20 cursor-pointer"
+          >
+            <Image src={rightArrow} alt="Right Arrow" />
+          </button>
+        </div>
       </div>
       <div className="flex items-center justify-center">
         <div className="bg-agsm-blue-200 rounded-md px-6 py-2 text-xl text-white">
