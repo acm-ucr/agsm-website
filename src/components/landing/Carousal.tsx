627ae600766004d428bc183a90afb2f88ce06c61
jason-ngn <bennguyen20122006@gmail.com>
2026-02-18 21:46:18 -0800

    fixed Carousal from comments

--- src/components/landing/Carousal.tsx
+++ src/components/landing/Carousal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import {
   Carousel,
   CarouselContent,
@@ -17,22 +17,40 @@ import rightArrow from "@/public/carousal/right_arrow.svg";
 import { useState } from "react";
 
 const Carousal = () => {
-  const imageArray = [Image1, Image2, Image3, Image4, Image5];
+  const imageArray: {
+    data: StaticImageData;
+    alt: string;
+  }[] = [
+    {
+      data: Image1,
+      alt: "A person standing in front of a slideshow",
+    },
+    {
+      data: Image2,
+      alt: "A group of people holding certificates",
+    },
+    {
+      data: Image3,
+      alt: "A group of people taking pictures together and giving a gift",
+    },
+    {
+      data: Image4,
+      alt: "A person giving a presentation",
+    },
+    {
+      data: Image5,
+      alt: "A group of people giving a presentation",
+    },
+  ];
 
   const [carouselApi, setCarouselApi] = useState<CarouselApi | null>(null);
 
   return (
-    <div className="p-8">
+    <div className="p-6">
       <div className="text-agsm-blue-200 flex items-center justify-center text-6xl font-bold">
         GALLERY
       </div>
-      <div className="relative mx-auto w-full overflow-hidden p-8">
-        {/* Left fade overlay */}
-        {/*<div className="from-agsm-yellow-100 pointer-events-none absolute top-0 left-0 z-10 h-full w-100 border-2 border-red-500 bg-gradient-to-r to-transparent"></div>*/}
-
-        {/* Right fade overlay */}
-        {/*<div className="from-agsm-yellow-100 pointer-events-none absolute top-0 right-0 z-10 h-full w-100 border-2 border-red-500 bg-gradient-to-l to-transparent"></div>*/}
-
+      <div className="relative my-8 w-full">
         <div className="absolute top-0 left-0 z-20 flex h-full w-16 items-center justify-start md:w-32">
           <div className="from-agsm-yellow-100 pointer-events-none absolute inset-0 bg-gradient-to-r to-transparent" />
           <button
@@ -52,13 +70,13 @@ const Carousal = () => {
           className="w-full"
         >
           <CarouselContent className="-ml-10">
-            {imageArray.map((image, index) => (
+            {imageArray.map(({ data, alt }, index) => (
               <CarouselItem
                 key={index}
                 className="basis-full pl-10 md:basis-1/3"
               >
                 <div className="relative overflow-hidden rounded-xl">
-                  <Image src={image} alt="Image" className="object-cover" />
+                  <Image src={data} alt={alt} className="block h-auto w-full" />
                 </div>
               </CarouselItem>
             ))}
