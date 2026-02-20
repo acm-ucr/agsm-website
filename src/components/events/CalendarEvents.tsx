09df2eef0cb8992a07ad75f113581da7d89d2e42
charlesluo06 <charlesluo06@gmail.com>
2026-02-20 01:12:02 -0800

    moved calendar into calendar events

---
+++ src/components/events/CalendarEvents.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { Calendar } from "@/components/ui/calendar";
+
+const CalendarEvents = () => {
+  return <Calendar className="mx-auto mt-15 w-1/2" />;
+};
+
+export default CalendarEvents;
