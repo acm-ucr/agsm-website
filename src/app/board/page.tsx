5d7e2961b795c4624f060a4e2e87b67c9753c346
paoloprogram12 <sethpsalazar@gmail.com>
2026-02-23 00:50:34 -0800

    mapped cards onto boards page and reformat

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -9,40 +9,40 @@ import Natalie from "@/public/board/Natalie.webp";
 import Kathiroli from "@/public/board/Kathiroli.webp";
 
 const executives = [
-  { image: Sheetal, name:"Sheetal Janala", position: "President"},
-  { image: Kazi, name:"Kazi Nahian", position: "Vice President"},
-  { image: Manshavi, name: "Manshavi Agarwal", position: "Vice President"}
+  { image: Sheetal, name: "Sheetal Janala", position: "President" },
+  { image: Kazi, name: "Kazi Nahian", position: "Vice President" },
+  { image: Manshavi, name: "Manshavi Agarwal", position: "Vice President" },
 ];
 
 const operations = [
-  { image: Natalie, name:"Natalie Trinh", position: "Director"},
-  { image: Kathiroli, name:"Kathiroli Vasu", position: "Executive"}
+  { image: Natalie, name: "Natalie Trinh", position: "Director" },
+  { image: Kathiroli, name: "Kathiroli Vasu", position: "Executive" },
 ];
 
 const Board = () => {
   return (
-    <div className="flex flex-col gap-16 w-screen items-center justify-center py-16">
+    <div className="flex w-screen flex-col items-center justify-center gap-16 py-16">
       <Header title="Executives" />
-      <div className="flex flex-row gap-16 w-full justify-center">
+      <div className="flex w-full flex-row justify-center gap-16">
         {executives.map((member) => (
-        <BoardCard 
-          key={member.name}
-          image={member.image}
-          name={member.name}
-          position={member.position}
-        />
-      ))}
+          <BoardCard
+            key={member.name}
+            image={member.image}
+            name={member.name}
+            position={member.position}
+          />
+        ))}
       </div>
       <Header title="Operations" />
-      <div className="flex flex-row gap-16 w-full justify-center">
+      <div className="flex w-full flex-row justify-center gap-16">
         {operations.map((member) => (
-        <BoardCard 
-          key={member.name}
-          image={member.image}
-          name={member.name}
-          position={member.position}
-        />
-      ))}
+          <BoardCard
+            key={member.name}
+            image={member.image}
+            name={member.name}
+            position={member.position}
+          />
+        ))}
       </div>
     </div>
   );
