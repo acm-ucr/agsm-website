0981f7910203992f6094cdc777d435e2d1b384d0
paoloprogram12 <sethpsalazar@gmail.com>
2026-02-23 00:48:24 -0800

    Mapped cards onto the boards page

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,9 +1,49 @@
 import Header from "@/components/board/Header";
+import BoardCard from "@/components/board/Card";
+// executives
+import Sheetal from "@/public/board/Sheetal.webp";
+import Kazi from "@/public/board/Kazi.webp";
+import Manshavi from "@/public/board/Manshavi.webp";
+// operations
+import Natalie from "@/public/board/Natalie.webp";
+import Kathiroli from "@/public/board/Kathiroli.webp";
+
+const executives = [
+  { image: Sheetal, name:"Sheetal Janala", position: "President"},
+  { image: Kazi, name:"Kazi Nahian", position: "Vice President"},
+  { image: Manshavi, name: "Manshavi Agarwal", position: "Vice President"}
+];
+
+const operations = [
+  { image: Natalie, name:"Natalie Trinh", position: "Director"},
+  { image: Kathiroli, name:"Kathiroli Vasu", position: "Executive"}
+];
 
 const Board = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex flex-col gap-16 w-screen items-center justify-center py-16">
       <Header title="Executives" />
+      <div className="flex flex-row gap-16 w-full justify-center">
+        {executives.map((member) => (
+        <BoardCard 
+          key={member.name}
+          image={member.image}
+          name={member.name}
+          position={member.position}
+        />
+      ))}
+      </div>
+      <Header title="Operations" />
+      <div className="flex flex-row gap-16 w-full justify-center">
+        {operations.map((member) => (
+        <BoardCard 
+          key={member.name}
+          image={member.image}
+          name={member.name}
+          position={member.position}
+        />
+      ))}
+      </div>
     </div>
   );
 };
