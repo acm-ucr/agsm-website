1709bdd04401392ab4b82ea14778796023b93f44
Larry Zhou <zhoularry15@gmail.com>
2026-02-24 08:15:16 -0800

    completed the marketing, outreach, finance, and communications mapping

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,9 +1,9 @@
-import Header from "@/components/board/Header";
+import BoardMap from "@/components/board/BoardMap";
 
 const Board = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <Header title="Executives" />
+    <div className="bg-agsm-yellow-100 flex h-screen w-screen flex-col items-center justify-center">
+      <BoardMap />
     </div>
   );
 };
---
+++ src/data/Board.ts
@@ -0,0 +1,27 @@
+import Maryam from "@/public/board/Maryam.webp";
+import Syed from "@/public/board/Syed.webp";
+import Kyle from "@/public/board/Kyle.webp";
+import Vishal from "@/public/board/Vishal.webp";
+
+export const board = [
+  {
+    image: Maryam,
+    name: "Maryam Noor",
+    position: "Director",
+  },
+  {
+    image: Syed,
+    name: "Syed Saif Ahmed",
+    position: "Director",
+  },
+  {
+    image: Kyle,
+    name: "Kyle Nishikawa",
+    position: "Director",
+  },
+  {
+    image: Vishal,
+    name: "Vishal Sharma",
+    position: "Director",
+  },
+];
