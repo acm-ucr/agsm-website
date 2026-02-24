1928d7478ead66570b3294dd52ba9d8ba09d1bfa
“Alaya” <alaya.sanai@icloud.com>
2026-02-23 19:24:25 -0800

    made the our vision section look good on both mobile and laptop

--- src/components/landing/OurVision.tsx
+++ src/components/landing/OurVision.tsx
@@ -7,15 +7,15 @@ const Vision = () => {
       <Image
         src={OurVisonImg}
         alt="Vision background"
-        className="pointer-events-none absolute top-40"
+        className="pointer-events-none absolute top-40 right-27 scale-45 md:left-0 md:scale-100"
       />
 
-      <div className="flex w-full justify-end">
+      <div className="flex w-screen justify-end text-right">
         <div className="flex max-w-2xl flex-col items-end text-right">
-          <div className="text-agsm-blue-200 mt-10 mr-35 text-6xl font-bold">
+          <div className="text-agsm-blue-200 mt-10 mr-10 text-3xl font-bold md:mr-35 md:text-6xl">
             OUR VISION
           </div>
-          <div className="mt-20 mr-35 text-xl text-[#3A3A3A]">
+          <div className="mt-10 mr-10 ml-42 text-lg text-[#3A3A3A] md:mt-20 md:mr-35 md:ml-0 md:text-xl">
             The AGSM Product Club strives to be the leading community for
             students aspiring to become successful product managers, providing
             them with the necessary resources, skills, and experience to launch
