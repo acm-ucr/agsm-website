a6c97cc7b1d77ed0cef08beb22431cf35fa851da
ManuChint <manu.chintalapathi@gmail.com>
2026-02-24 06:12:22 -0800

    added what we offer and adjusted card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import AboutUs from "@/components/landing/AboutUs";
+import WhatWeOffer from "@/components/landing/WhatWeOffer";
 import OurVision from "@/components/landing/OurVision";
 import Carousal from "@/components/landing/Carousal";
 
 const Home = () => {
   return (
-    <div className="bg-agsm-yellow-100 font-agsm-main flex min-h-screen w-full flex-col">
+    <div className="font-agsm-main flex min-h-screen w-full flex-col">
       <AboutUs />
+      <WhatWeOffer />
       <OurVision />
       <Carousal />
     </div>
