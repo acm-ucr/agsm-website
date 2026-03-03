a7ed97f4afb9d885360db2fe497246920db6eb46
ManuChint <manu.chintalapathi@gmail.com>
2026-03-02 17:55:06 -0800

    adjusted attributes of card and added animations to whatweoffer

--- src/components/landing/Card.tsx
+++ src/components/landing/Card.tsx
@@ -12,8 +12,12 @@ const Card = ({ Icon, title, description }: CardProps) => {
           <Icon className="bg-agsm-blue-100 text-agsm-blue-200 h-18 w-18 rounded-md p-4" />
         )}
       </div>
-      <div className="text-agsm-blue-200 my-5 text-3xl">{title}</div>
-      <div className="text-aneutral-800 text-2xl font-thin">{description}</div>
+      <div className="text-agsm-blue-200 my-5 text-3xl select-none">
+        {title}
+      </div>
+      <div className="text-aneutral-800 text-2xl font-thin select-none">
+        {description}
+      </div>
     </div>
   );
 };
