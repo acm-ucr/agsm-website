d942b3b6a59c27c6d41f3e4559721264d774b437
rgran035-cs <rgran035@ucr.edu>
2026-03-09 18:55:02 -0700

    Animated and Responsified Board

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,11 +1,20 @@
+"use client";
+import { motion } from "motion/react";
 import BoardMap from "@/components/board/BoardMap";
 import Header from "@/components/Header";
 
 export default function BoardPage() {
   return (
     <div className="flex w-full flex-col">
+      <motion.div
+        initial={{ opacity: 0, y: 15 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: false, amount: 0.1}}
+        transition={{duration: 0.8}}
+        >
       <Header text={"MEET THE BOARD"} />
-      <BoardMap />
+    </motion.div>
+    <BoardMap />
     </div>
   );
 }
--- src/components/board/BoardMap.tsx
+++ src/components/board/BoardMap.tsx
@@ -1,26 +1,50 @@
+"use client";
+import { motion } from "motion/react";
 import Card from "@/components/board/Card";
 import Header from "@/components/board/Header";
 import { board } from "@/data/Board";
+
 import { executives } from "@/data/Board";
 import { operations } from "@/data/Board";
+
+const titleVariants = {
+  hidden: { opacity: 0, x: -50},
+  visible: { opacity: 1, x: 0},
+};
 
 const BoardMap = () => {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center py-8">
       <div className="flex w-screen flex-col items-center justify-center gap-16 py-16">
+        <motion.div
+            variants={titleVariants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once:false, amount:0.5}}
+            transition={{duration:0.8, ease: "easeOut"}}
+            >
         <Header title="Executives" />
-        <div className="flex w-full flex-row justify-center gap-16">
+        </motion.div>
+        <div className="flex flex-wrap w-full flex-row justify-center gap-16">
+      
           {executives.map((member) => (
             <Card
-              key={member.name}
               image={member.image}
               name={member.name}
               role={member.role}
             />
           ))}
         </div>
+        <motion.div
+            variants={titleVariants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once:false, amount:0.5}}
+            transition={{duration:0.8, ease: "easeOut"}}
+            >
         <Header title="Operations" />
-        <div className="flex w-full flex-row justify-center gap-16">
+        </motion.div>
+        <div className="flex flex-wrap w-full flex-row justify-center gap-16">
           {operations.map((member) => (
             <Card
               key={member.name}
@@ -31,10 +55,18 @@ const BoardMap = () => {
           ))}
         </div>
       </div>
-      <div className="mx-auto grid w-full max-w-4xl grid-cols-2 gap-6 px-4">
-        <div className="flex flex-col items-center gap-2">
+      <div className="mx-auto grid w-full max-w-4xl grid-cols-1 md:grid-cols-2 gap-4">
+<div className="flex flex-col items-center gap-2">
+  <motion.div
+            variants={titleVariants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once:false, amount:0.5}}
+            transition={{duration:0.8, ease: "easeOut"}}
+            >
           <Header title="Marketing" />
-          <div className="flex w-full flex-row justify-center gap-2">
+          </motion.div>
+          <div className="flex w-full flex-row justify-center gap-2 -ml-18">
             {board.slice(0, 1).map(({ image, name, position }, index) => (
               <div className="box-border w-48 p-2">
                 <Card key={index} name={name} role={position} image={image} />
@@ -43,8 +75,16 @@ const BoardMap = () => {
           </div>
         </div>
         <div className="flex flex-col items-center gap-2">
+          <motion.div
+            variants={titleVariants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once:false, amount:0.5}}
+            transition={{duration:0.8, ease: "easeOut"}}
+            >
           <Header title="Outreach" />
-          <div className="flex w-full flex-row justify-center gap-2">
+          </motion.div>
+          <div className="flex w-full flex-row justify-center gap-2 -ml-18">
             {board.slice(1, 2).map(({ image, name, position }, index) => (
               <div className="box-border w-48 p-2">
                 <Card key={index} name={name} role={position} image={image} />
@@ -53,8 +93,16 @@ const BoardMap = () => {
           </div>
         </div>
         <div className="flex flex-col items-center gap-2">
+          <motion.div
+            variants={titleVariants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once:false, amount:0.5}}
+            transition={{duration:0.8, ease: "easeOut"}}
+            >
           <Header title="Finance" />
-          <div className="flex w-full flex-row justify-center gap-2">
+          </motion.div>
+          <div className="flex w-full flex-row justify-center gap-2 -ml-18">
             {board.slice(2, 3).map(({ image, name, position }, index) => (
               <div className="box-border w-48 p-2">
                 <Card key={index} name={name} role={position} image={image} />
@@ -63,8 +111,16 @@ const BoardMap = () => {
           </div>
         </div>
         <div className="flex flex-col items-center gap-2">
+          <motion.div
+            variants={titleVariants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once:false, amount:0.5}}
+            transition={{duration:0.8, ease: "easeOut"}}
+            >
           <Header title="Communications" />
-          <div className="flex w-full flex-row justify-center gap-2">
+          </motion.div>
+          <div className="flex w-full flex-row justify-center gap-2 -ml-18">
             {board.slice(3, 4).map(({ image, name, position }, index) => (
               <div className="box-border w-48 p-2">
                 <Card key={index} name={name} role={position} image={image} />
