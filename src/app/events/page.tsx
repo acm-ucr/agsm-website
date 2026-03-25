8fb06c147243a03fbde61926c6fb8cbdefb54975
ManuChint <manu.chintalapathi@gmail.com>
2026-03-25 02:43:52 -0700

    responsified and animated upcoming events and added to pages

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -4,11 +4,13 @@ import CalendarEvents from "@/components/events/CalendarEvents";
 import PastEvents from "@/components/events/PastEvents";
 import Header from "@/components/Header";
 import Title from "@/components/board/Header";
+import UpcomingEvents from "@/components/events/UpcomingEvents";
 
 const Events = () => {
   return (
     <div className="font-agsm-main n flex w-screen flex-col">
       <Header text={"EVENTS"} />
+      <UpcomingEvents />
       <CalendarEvents />
       <Title title={"PAST EVENTS"} />
       <PastEvents />
